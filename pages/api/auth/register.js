b9c43e32d13d10a5ceb62efb48e56e5503599e9d
AdakHaddad <muhammadmuqtada@gmail.com>
2023-07-14 23:51:45 +0700

    Navvv

--- pages/api/auth/register.js
+++ pages/api/auth/register.js
@@ -1,36 +1,36 @@
-import { hashPassword } from "../../../lib/auth";
-import { connectToDatabase } from "../../../lib/db";
+// import { hashPassword } from "../../../lib/auth";
+// import { connectToDatabase } from "../../../lib/db";
 
-export default async function handler(req, res) {
-  if (req.method !== "POST") {
-    return res.status(400).json({ message: "Invalid request method" });
-  }
+// export default async function handler(req, res) {
+//   if (req.method !== "POST") {
+//     return res.status(400).json({ message: "Invalid request method" });
+//   }
 
-  const { username, password, name, age } = req.body;
+//   const { username, password, name, age } = req.body;
 
-  if (!username || !password || !name || !age) {
-    return res.status(422).json({ message: "Invalid input" });
-  }
+//   if (!username || !password || !name || !age) {
+//     return res.status(422).json({ message: "Invalid input" });
+//   }
 
-  const client = await connectToDatabase();
-  const usersCollection = client.db().collection("users");
+//   const client = await connectToDatabase();
+//   const usersCollection = client.db().collection("users");
 
-  const existingUser = await usersCollection.findOne({ username });
-  if (existingUser) {
-    client.close();
-    return res.status(422).json({ message: "Username already exists" });
-  }
+//   const existingUser = await usersCollection.findOne({ username });
+//   if (existingUser) {
+//     client.close();
+//     return res.status(422).json({ message: "Username already exists" });
+//   }
 
-  const hashedPassword = await hashPassword(password);
+//   const hashedPassword = await hashPassword(password);
 
-  const result = await usersCollection.insertOne({
-    username,
-    password: hashedPassword,
-    name,
-    age,
-  });
+//   const result = await usersCollection.insertOne({
+//     username,
+//     password: hashedPassword,
+//     name,
+//     age,
+//   });
 
-  client.close();
+//   client.close();
 
-  res.status(201).json({ message: "User registered successfully" });
-}
+//   res.status(201).json({ message: "User registered successfully" });
+// }
